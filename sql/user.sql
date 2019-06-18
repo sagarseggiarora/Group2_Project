dc9567d91a5b80997ec905e46e8d509526c3d0cb
dashmeetsingh86 <singhd11@student.douglascollege.ca>
2019-06-17 23:56:25 -0700

    Added User entity class

---
+++ sql/user.sql
@@ -0,0 +1,18 @@
+create table user (
+	user_id INT AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	address VARCHAR(50),
+	phone_number VARCHAR(50)
+);
+insert into user (user_id, first_name, last_name, email, address, phone_number) values (1, 'Georgia', 'Lortzing', 'glortzing0@hatena.ne.jp', '25 Sachtjen Street', '363-922-6988');
+insert into user (user_id, first_name, last_name, email, address, phone_number) values (2, 'Oby', 'McIlenna', 'omcilenna1@spiegel.de', '6686 Old Shore Hill', '958-703-8988');
+insert into user (user_id, first_name, last_name, email, address, phone_number) values (3, 'Devin', 'Browell', 'dbrowell2@phoca.cz', '82 Arapahoe Circle', '211-515-1412');
+insert into user (user_id, first_name, last_name, email, address, phone_number) values (4, 'Rudolfo', 'Fay', 'rfay3@ifeng.com', '659 Walton Way', '422-730-7767');
+insert into user (user_id, first_name, last_name, email, address, phone_number) values (5, 'Colet', 'Emmanuele', 'cemmanuele4@nymag.com', '1 Stephen Junction', '952-135-1303');
+insert into user (user_id, first_name, last_name, email, address, phone_number) values (6, 'Carlye', 'Degoix', 'cdegoix5@mit.edu', '88720 Upham Circle', '516-898-0589');
+insert into user (user_id, first_name, last_name, email, address, phone_number) values (7, 'Patrizio', 'Drysdale', 'pdrysdale6@vkontakte.ru', '9 Hermina Circle', '701-333-6218');
+insert into user (user_id, first_name, last_name, email, address, phone_number) values (8, 'Jelene', 'Woodhall', 'jwoodhall7@dailymotion.com', '9 Forest Park', '205-801-7865');
+insert into user (user_id, first_name, last_name, email, address, phone_number) values (9, 'Lian', 'Matisoff', 'lmatisoff8@gov.uk', '1643 Tomscot Pass', '395-566-4438');
+insert into user (user_id, first_name, last_name, email, address, phone_number) values (10, 'Federico', 'Ewert', 'fewert9@globo.com', '481 Bluejay Drive', '243-672-6985');
---
+++ src/com/project/Entity/User_Group2.java
@@ -0,0 +1,63 @@
+package com.project.Entity;
+
+import java.util.Vector;
+
+public class User_Group2 {
+	private int user_id = 0;
+	private String first_name = "";
+	private String last_name = "";
+	private String email = "";
+	private String address = "";
+	private String phone_number = "";
+	
+	
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+	
+	public Vector toVector() {
+		Vector v = new Vector();
+		
+		v.add(user_id);
+		v.add(first_name);
+		v.add(last_name);
+		v.add(email);
+		v.add(address);
+		v.add(phone_number);
+		return v;
+	}
+	
+}
