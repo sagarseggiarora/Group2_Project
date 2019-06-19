85ebc6f9e43d7fb2385a0bf16cacff248c8605c9
Sagar Arora <Sagar.seggi@gmail.com>
2019-06-19 01:22:51 -0700

    Updated DAO class by Adding newTicket method and newStudent Method

--- src/com/project/Boundary/UserDAO.java
+++ src/com/project/Boundary/UserDAO.java
@@ -7,6 +7,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+
+
+import com.project.Entity.Tickets_Group2;
 import com.project.Entity.User_Group2;
 
 public class UserDAO {
@@ -58,9 +61,10 @@ public class UserDAO {
 			System.out.println(sx.getSQLState());
 			}
 		}
-		public boolean  validate(String username, String Password)
+	
+		public boolean validate(String username, String Password)
 		{
-			String sql ="select username,password from login where username= '"+username+"' and password= '"+password+"'";
+			String sql ="select username,password from login where username = '"+username+"' and password= '"+Password+"'";
 			boolean check = false;
 			
 			try {
@@ -87,6 +91,58 @@ public class UserDAO {
 				System.out.println(sx.getSQLState());
 			}
 			return check;
+			
+		}
+		
+		public int newTicket(Tickets_Group2 tick) {
+			
+			int ticketNumber=0;
+			String sql="INSERT INTO Tickets (email, issue)" + " VALUES ('" + tick.getEmail()+"','"+tick.getIssue()+"');";
+			
+			try {
+				ConnectDB();
+				stmt = conn.createStatement();
+				ticketNumber = stmt.executeUpdate(sql,Statement.RETURN_GENERATED_KEYS);
+				DisconnectDB();
+			}
+			catch(SQLException sx)
+			{
+				System.out.println("Error connection to database");
+				System.out.println(sx.getMessage());
+				System.out.println(sx.getErrorCode());
+				System.out.println(sx.getSQLState());
+			}
+			return ticketNumber;
+			
+		}
+		
+		public int newStudent(User_Group2 cust)	{
+			
+			int newCustID = 0;
+			
+			try {
+				
+				ConnectDB();
+				
+				//Prepare the statement to save to the database.
+				String sql = "INSERT INTO user (first_name, last_name, email,address,phone_number)"
+						+ " VALUES ('" + cust.getFirst_name() + "','" + cust.getLast_name() + "','" + cust.getEmail() + "','" + cust.getAddress() + "','" + cust.getPhone_number() + "');";
+				
+				//Create the statement
+				this.stmt = this.conn.createStatement();
+				
+				//Execute the statement
+				newCustID = stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+
+				DisconnectDB();
+			} catch (SQLException sx)	{
+				System.out.println("Error Inserting Student");
+				System.out.println(sx.getMessage());
+				System.out.println(sx.getErrorCode());
+				System.out.println(sx.getSQLState());
+			}
+
+			return newCustID;
 		}
 
 
